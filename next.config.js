fee3bd4464efa8fea1a4ae7b8dc7d0d0973588bd
DESKTOP-6SCC3DF\user <khoabk12@gmail>
2024-07-28 15:57:35 +0700

    edit basePath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/developer-portfolio",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
