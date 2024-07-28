3921d148f93bf1d0c0d185f40dadca5fdc3aac35
DESKTOP-6SCC3DF\user <khoabk12@gmail>
2024-07-28 16:29:43 +0700

    edit base path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath:"/app",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
